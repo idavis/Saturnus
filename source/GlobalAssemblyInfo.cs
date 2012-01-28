a4360c03d4912a668ce7456411752311782c3c35
Ian Davis <ian@innovatian.com>
2012-01-28 09:02:59 -0800

    Adding chewie nuget file, gitignore, and global AssemblyInfo

---
+++ source/GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyCompany("Innovatian Software")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Innovatian Software 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
